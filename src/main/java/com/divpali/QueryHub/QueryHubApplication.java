02781c4015e5b946c1c03428c1efdac6836788d4
Divya Paliwal <div.pali@gmail.com>
2024-05-11 16:46:22 +0530

    first commit

---
+++ src/main/java/com/divpali/QueryHub/QueryHubApplication.java
@@ -0,0 +1,13 @@
+package com.divpali.QueryHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divpali/QueryHub/QueryHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divpali.QueryHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
